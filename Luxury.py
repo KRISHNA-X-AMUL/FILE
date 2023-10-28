3c2814e4c596c22e076c4a9bdcceef7f79ba5ecf
KRISHNA-X-AMUL <140779732+KRISHNA-X-AMUL@users.noreply.github.com>
2023-10-28 12:10:45 +0545

    Update Luxury.py

--- Luxury.py
+++ Luxury.py
@@ -3,8 +3,6 @@ try:os.system('')
 except:pass
 try:os.system('git pull')
 except:pass
-try:os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
-except:pass
 try:
     __import__("acc").___AMUL_XD_()
 except Exception as e:
